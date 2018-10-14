40aa0c471cc36e4edd3720e8cdc8ad5ac3f817f1
natejahnke <nathanjjahnke@gmail.com>
2018-10-14 18:52:59 -0500

    first commit, working on fighter card

---
+++ src/components/FighterCardSmall.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class FighterCardSmall extends React.Component {
+  state = {
+    fighter: undefined
+  };
+  render() {
+    return (
+      <div>
+        <img
+          src="../../public/images/khabib_face.png"
+
+        />
+      </div>
+    );
+  }
+}
