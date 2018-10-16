7e5532e3ab624d23094eebd01f8fcad16cad064f
Jahnke <nathan.jahnke@tsi.corp.com>
2018-10-16 09:33:27 -0500

    made a 4 column grid for fighters and broke full name on two lines

--- .history/src/components/FighterCardSmall_20181015162354.js
+++ .history/src/components/FighterCardSmall_20181015162354.js
@@ -6,9 +6,9 @@ const FighterCardSmall = (props) => (
         src={props.image} 
         className="fighter-card-image"
         />
-        {/* <div className="fighter-card-image-rank">1</div> */}
+        <div className="fighter-card-image-rank">{"1"}</div>
         <div className="fighter-card-body">
-          <h1 className="fighter-card-body-name">{props.first_name}<br/>{props.last_name}</h1>
+          <h1 className="fighter-card-body-name">{props.name}</h1>
           <p className="fighter-card-body-record">{props.wins}-{props.losses}<span className="flag-icon flag-icon-ru"></span></p>
           
         </div>
--- .history/src/components/FighterCardSmall_20181016085412.js
+++ .history/src/components/FighterCardSmall_20181016085412.js
@@ -8,7 +8,7 @@ const FighterCardSmall = (props) => (
         />
         {/* <div className="fighter-card-image-rank">1</div> */}
         <div className="fighter-card-body">
-          <h1 className="fighter-card-body-name">{props.first_name}<br/>{props.last_name}</h1>
+          <h1 className="fighter-card-body-name">{props.name}</h1>
           <p className="fighter-card-body-record">{props.wins}-{props.losses}<span className="flag-icon flag-icon-ru"></span></p>
           
         </div>
